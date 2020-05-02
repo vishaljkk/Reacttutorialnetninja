4aa6a7bd01a8ef0555261670e968647134d8e195
vishaljkk <vishal.kk@somaiaya.edu>
2020-05-02 20:47:35 +0530

    work till now

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    ninjas: [
+      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
+      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
+      { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
+    ]
+  }
+  addNinja = (ninja)=>{
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas,ninja];
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+  render(){
+    return (
+      <div className="App">
+        <h1>My first React app!</h1>
+        <p>Welcome :)</p>
+        <Ninjas ninjas={this.state.ninjas}/>
+        <AddNinja addNinja={this.addNinja}/>
+      </div>
+    );
+  }
 }
 
 export default App;
