3485b1e7435048428c52237cd9c611cf9b40080c
vishaljkk <vishal.kk@somaiaya.edu>
2020-05-03 20:22:30 +0530

    withactive NacLink class

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
   
 import React from 'react';
-
+import { Link, NavLink } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav className="nav-wrapper red darken-3">
       <div className="container">
         <ul className="right">
-          <li><a href="/">Home</a></li>
-          <li><a href='/about'>About</a></li>
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to='/about'>About</NavLink></li>
         </ul>
       </div>
     </nav> 
